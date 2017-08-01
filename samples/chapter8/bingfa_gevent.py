1d512e03ba20965c194fc43804ffa126732b53ac
wzgavid <wzgdavid@sina.com>
2017-08-01 15:08:59 +0800

    fff

--- samples/chapter8/bingfa_gevent.py
+++ samples/chapter8/bingfa_gevent.py
@@ -5,18 +5,14 @@ import time
 def foo(n):
     print(n)
     time.sleep(5)  # 好比服务器的响应时间
-    
-#
-#
 tasks = [
     gevent.spawn(foo, '123'),
-    gevent.spawn(foo, '123'),
-    gevent.spawn(foo, '123'),
-    gevent.spawn(foo, '123'),
-    gevent.spawn(foo, '123')
+    gevent.spawn(foo, '124'),
+    gevent.spawn(foo, '125'),
+    gevent.spawn(foo, '126'),
+    gevent.spawn(foo, '127')
 ]
-
-gevent.joinall(tasks)
+#gevent.joinall(tasks)
 
 # 不用并发，费时
 foo('567')
