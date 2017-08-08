3d30ed25178d8551f328257587bd7b3e47fc70fe
wzgavid <wzgdavid@sina.com>
2017-08-08 08:56:27 +0800

    crawl 51job 数据分析

---
+++ samples/chapter8/crawl_bigdata/crawl_bigdata51.py
@@ -0,0 +1,107 @@
+'''
+从51job抓取title包含'数据分析'的职位信息
+'''
+
+from urllib import request, error
+from lxml import etree
+import json
+import csv
+import gevent
+from gevent import monkey; monkey.patch_all()
+
+
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36',
+    "Referer": "http://search.51job.com/",
+    'Connection': 'keep-alive'
+}
+
+def get_html(url):
+
+    req = request.Request(url, headers=headers)
+    try:
+        #response = request.urlopen(url)
+        response = request.urlopen(req)
+        html = response.read().decode('gbk')
+    except error.URLError as e:
+        print(e)
+        html = None
+    except error.HTTPError as e:
+        print(e)
+        html = None
+    
+    #print(html)
+    #with open('51jobs.html', 'w') as f: # 第一次执行，保存为本地文件
+    #    f.write(html)
+    return html
+
+
+def crawl_data(html):
+    html = etree.HTML(html)
+    html = etree.ElementTree(html)
+
+    items = html.xpath('//div[@id="resultList"]/div[@class="el"]')
+    rows = []
+    for i, item in enumerate(items):
+        item = etree.ElementTree(item)
+        print(i)
+        #title = item.xpath('//p[@class="t1 "]/span/a/@title')[0]
+        #有些带图标的p的类名不是t1，是"t1 tg", 所以用contain
+        title = item.xpath('//p[contains(@class,"t1")]/span/a/@title')[0]
+        link = item.xpath('//p[contains(@class,"t1")]/span/a/@href')[0]
+        company = item.xpath('//span[@class="t2"]/a/@title')[0]
+        area = item.xpath('//span[@class="t3"]/text()')[0]
+        salary = item.xpath('//span[@class="t4"]/text()')
+        salary = salary[0] if salary else None
+        #print(i, salary)
+        # 经验和学历
+        
+        detail = get_html(link)
+        if detail:
+            detail = etree.ElementTree(etree.HTML(detail))
+            exp = detail.xpath('//div[@class="jtag inbox"]//em[@class="i1"]/following::text()[1]')
+            #exp = detail.xpath('//div[@class="t1"]/span/em[@class="i1"]/parent::span/text()')
+            exp = exp[0] if exp else None
+            xueli = detail.xpath('//div[@class="jtag inbox"]//em[@class="i2"]/following::text()[1]')
+            #xueli = detail.xpath('//div[@class="t1"]/span/em[@class="i2"]/parent::span/text()')
+            xueli = xueli[0] if xueli else None
+        else:
+            continue
+        #print(i, exp, xueli)
+        row = (title, link, company, area, salary, exp, xueli)
+        rows.append(row)
+    return rows
+
+
+def save_to_csv(data, filename, mode):
+    #for n in data:
+    #    print(n)
+    with open(filename, mode, newline="") as datacsv:
+        writer = csv.writer(datacsv)
+        # 写入标题
+        if mode =='w':
+            writer.writerow(["职位名","链接", "公司","工作地点","薪资","经验","学历"])
+        # 写数据
+        writer.writerows(data)
+
+
+def crawl_one(url):
+    #url = 'http://search.51job.com/list/020000,000000,0000,00,9,99,%2520,2,{}.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='.format(1) # format里页数
+
+    html = get_html(url)
+    #html = local_html()  # 先用本地保存的html边调试边写代码
+    if html:
+        data = crawl_data(html)
+        save_to_csv(data, r'51jobs.csv', mode='a')
+
+def run():
+    tasks = []
+    url = 'http://search.51job.com/list/000000,000000,0000,00,9,99,%25E6%2595%25B0%25E6%258D%25AE%25E5%2588%2586%25E6%259E%2590,2,{}.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
+    for n in range(40, 57):
+        tasks.append(gevent.spawn(crawl_one, url.format(n)))
+    gevent.joinall(tasks)
+
+if __name__ == '__main__':
+    #url = 'http://search.51job.com/list/000000,000000,0000,00,9,99,python,2,{}.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=1&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='.format(1)
+    #crawl_one(url)
+    #run()
---
+++ samples/chapter8/crawl_bigdata/data_scan51.py
@@ -0,0 +1,141 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def _salary(salary):
+    '''薪资统一以月计算，取均值'''
+    # 6-8千/月 2-6万/月  2.5-5万/月  0.8-1.5万/月   150元/天   20-30万/年  
+
+    if salary is np.nan:# nan值不处理
+        return
+    elif salary.endswith('万/月'):
+        salary = salary.replace('万/月', '')
+        s = salary.split('-')
+        rtn = (float(s[0]) + float(s[1]))*10000 / 2
+    elif salary.endswith('千/月'):
+        salary = salary.replace('千/月', '')
+        s = salary.split('-')
+        rtn = (float(s[0]) + float(s[1]))*1000 / 2
+    elif salary.endswith('万/年'):
+        salary = salary.replace('万/年', '')
+        s = salary.split('-')
+        rtn = (float(s[0]) + float(s[1]))*10000 / 2 / 12
+    elif salary.endswith('元/天'):
+        salary = salary.replace('元/天', '')
+        rtn = float(salary) * 21.75
+
+    return round(rtn)
+
+def _exp(exp):
+    if exp is np.nan:# nan值不处理
+        return
+    if exp == '无工作经验':
+        rtn = 0
+    elif exp.startswith('10年'):
+        rtn = 10
+    else:
+        rtn = int(exp[:1])
+    return rtn
+#print(_exp('10年以上经验'))
+
+def _area(area):
+    if area is np.nan:# nan值不处理
+        return
+    elif '-' in area:
+        rtn =  area.split('-')[0]
+    else:
+        rtn = area
+    return rtn
+
+
+sns.set_style("whitegrid")# darkgrid , whitegrid , dark , white ,和 ticks 
+plt.rcParams['font.sans-serif'] = ['SimHei'] # 正常显示中文
+df =  pd.read_csv(r'data_51jobs.csv', encoding='gbk')
+#df = df.dropna()
+df['工作地点'] = df['工作地点'].apply(_area)
+df['薪资'] = df['薪资'].apply(_salary)
+df['经验'] = df['经验'].apply(_exp)
+
+#df.to_csv('data_51jobs2.csv')
+
+# 职位数量城市分布饼图， 以dropna前的数据画
+#print(df['工作地点'].value_counts())
+# 各地区数量饼图
+def pie1():
+    n = 8
+    cnt = df['工作地点'].value_counts()
+    cnt = cnt.drop('异地招聘')
+    print(cnt)
+    labels = list(cnt.index[:n]) + ['其他']
+    sizes = list(cnt.values[:n]) + [cnt.values[n:].sum()]
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+    plt.axis('equal') # 轴相等，即圆形
+    plt.title("不同地区职位比例")
+    plt.show()
+#pie1()
+
+# 各经验数量饼图
+def pie2():
+    cnt = df['经验'].value_counts()[:5]
+    print(cnt)
+    labels = pd.Series(cnt.index).apply(lambda x: str(x)+'年以上')
+    sizes = cnt.values
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+    plt.axis('equal') # 轴相等，即圆形
+    plt.title("不同经验要求职位比例")
+    plt.show()
+#pie2()
+
+
+df = df.dropna()
+#print(df.head(20))
+#print(df.shape[0])
+print(df.describe())
+
+
+
+
+# 全国月薪柱状分布图
+# 以城市分
+#df = df[df['工作地点'] ==  '深圳']
+#plt.title('数据分析职位月薪(深圳)')
+#bins=range(0,40000,5000) # 范围0到200，每个柱的宽度20
+## hist画柱状图
+#plt.hist(df["薪资"],bins,color='#3333ee',width=4000) 
+#plt.xlabel('月薪')
+#plt.ylabel('计数')
+#plt.text(1, 10000,'sdfsdf')
+#plt.plot()
+## 平均值
+#plt.axvline(df['薪资'].mean(),linestyle='dashed',color='red')
+#plt.show()
+
+
+# 主要城市盒形图
+#main_cities = df['工作地点'].value_counts()[:10]
+#main_cities = main_cities.drop('异地招聘')
+#dfbox = df[df['工作地点'].isin(main_cities.index)]
+##print(dfbox.head())
+#plt.title('主要城市盒形图')
+#sns.boxplot(x="工作地点", y = '薪资', data = dfbox)
+#plt.ylim(0,40000)
+#plt.show()
+
+
+
+# 经验月薪盒形图
+#plt.title('经验薪资盒形图')
+#sns.boxplot(x="经验", y = '薪资', data = df)
+#plt.ylim(0,50000)
+##plt.text('sfsf')
+#plt.show()
+
+
+# 学历月薪盒形图
+plt.title('学历月薪盒形图')
+sns.boxplot(x="学历", y = '薪资', data = df)
+plt.ylim(0,50000)
+#plt.text('sfsf')
+plt.show()
+#print(df.describe())
