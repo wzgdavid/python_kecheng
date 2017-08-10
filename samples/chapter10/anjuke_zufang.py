647aa94aa5f7c7d6babd506f25da4ca626035d3e
wzgavid <wzgdavid@sina.com>
2017-08-10 16:17:02 +0800

    recommend zufang

--- samples/chapter10/anjuke_zufang.py
+++ samples/chapter10/anjuke_zufang.py
@@ -15,31 +15,33 @@ def recommend()
   如果accept了几条房源的话，看属于哪个类别的房源最多
 4 recommend函数就推荐那个最多类别的房源，推5条左右
 
-简单起见，这里只考虑，租金，装修，面积，租赁方式
+简单起见，这里只考虑，租金，装修，面积
 '''
-import copy
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans, DBSCAN
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-import seaborn as sns
+from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
-sns.set_style("whitegrid")
+from random import choice
+
 
 df = pd.read_csv(r'..\csv\anjuke.csv', encoding='gbk')
-#print(df)
-print(df.columns)
-X = df.loc[:, ['租金', '租赁方式', '装修', '面积', '年代']]
+#print(df.tail(10))
+#print(df.columns)
+#X = df.loc[:, ['租金', '租赁方式', '装修', '面积', '年代']]
+X = df.loc[:, ['租金', '装修', '面积']]
 # 特征整理
 def get_mianji(mianji):
     return mianji.replace('平米', '')
-def get_niandai(niandai):
-    if niandai == "暂无":
-        rtn = None
-    else:
-        rtn = niandai.replace('年', '')
-    return rtn
+#def get_niandai(niandai):
+#    if niandai == "暂无":
+#        rtn = 2000
+#    else:
+#        rtn = niandai.replace('年', '')
+#    return int(rtn) - 1980
+
 # 装修用手工整理，安装装修的简单到豪华排序，LabelEncoder的顺序不一定，所以不用
 zx = X['装修'].copy()
 zx[zx=='毛坯'] = 1
@@ -49,26 +51,84 @@ zx[zx=='精装修'] = 4
 zx[zx=='豪华装修'] = 5
 X['装修'] = zx
 X['面积'] = X['面积'].apply(get_mianji)
-X['年代'] = X['年代'].apply(get_niandai)
-X['租赁方式'] = LabelEncoder().fit_transform(X['租赁方式'].values)
+#X['年代'] = X['年代'].apply(get_niandai)
+#X['租赁方式'] = LabelEncoder().fit_transform(X['租赁方式'].values)
 X = X.dropna()
-print(X.head(20))
+#print(X.head(50), X.shape)
 ss = StandardScaler()
 X2 = ss.fit_transform(X)
-kmeans = KMeans(n_clusters=10, max_iter=100)
+kmeans = KMeans(n_clusters=15, max_iter=500)
 kmeans.fit(X2)
 
 #统计各个类别的数目
-r1 = pd.Series(kmeans.labels_).value_counts()
-# 找出聚类中心
-#r2 = pd.DataFrame(kmeans.cluster_centers_)
-# 聚类中心真实值
-r2 = pd.DataFrame(ss.inverse_transform(kmeans.cluster_centers_))
-#横向连接（0是纵向），得到聚类中心对应的类别下的数目
-r = pd.concat([r2, r1], axis = 1) 
-#重命名表头
-r.columns = list(X.columns) + ['类别数目']
-print(r)
+#r1 = pd.Series(kmeans.labels_).value_counts()
+## 找出聚类中心
+##r2 = pd.DataFrame(kmeans.cluster_centers_)
+## 聚类中心真实值
+#r2 = pd.DataFrame(ss.inverse_transform(kmeans.cluster_centers_))
+##横向连接（0是纵向），得到聚类中心对应的类别下的数目
+#r = pd.concat([r2, r1], axis = 1) 
+##重命名表头
+#r.columns = list(X.columns) + ['类别数目']
+#print(r)
 
 
 y = kmeans.predict(X2)
+ydata = pd.DataFrame(y, columns=['分类'])
+# knn学习用
+data = pd.concat([X, ydata], axis=1)
+# 推荐用
+data_recommend = pd.concat([df, ydata], axis=1)
+
+#print(data.head(30), data.shape[0])
+X = data.drop('分类', axis=1)
+y = data['分类']
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X, y)
+
+# 假装这是某个用户最近的浏览历史记录
+viewed = [
+    [4000,  4, 50],
+    [4000,  4, 50],
+    [4000,  4, 50],
+    [4000,  3, 50],
+    [4200,  4, 60],
+    [4400,  4, 80],
+    [4000,  3, 50],
+    [4000,  4, 60],
+    [42000, 4, 120],
+    [4000,  4, 90],
+    [4500,  2, 27],
+    ]
+
+
+def recommend(viewed):
+    '''
+    根据用户最近的浏览记录，推荐浏览类型最多的房源
+    '''
+    viewed_types = knn.predict(viewed)
+    #print(viewed_types)
+    value_counts = pd.Series(viewed_types).value_counts()
+    #print(value_counts)
+    most_view = value_counts.index[0] # 推荐最多浏览的类型
+    print(most_view)
+    recommended = data_recommend[data_recommend['分类'] == most_view]
+    #idx = np.array([1,2,3,4,5])
+    n = 5
+    if recommended.shape[0]>n: # 在这一类别里随机选5条，否则就全部选出
+        idx = list(recommended.index)
+        choiced_idx = []
+        while n > 0:
+            a = choice(idx)
+            choiced_idx.append(a)
+            idx.remove(a)
+            
+            n -= 1
+    choiced_idx = np.array(choiced_idx)
+    #print(choiced_idx)
+    recommended = recommended.ix[choiced_idx, :]
+    print(recommended)
+    return recommended
+
+
+recommend(viewed)
