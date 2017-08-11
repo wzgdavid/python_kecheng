283c31ce89181403140d442d255745be9f1d4e16
wzgavid <wzgdavid@sina.com>
2017-08-11 10:59:29 +0800

    anjuke tuijian

--- samples/chapter10/anjuke_zufang.py
+++ samples/chapter10/anjuke_zufang.py
@@ -11,8 +11,8 @@ def recommend()
 初步设想思路是
 1 先用聚类分好类，产生data
 2 然后用knn（K最近邻）算法，去学习上述分好类的data，产生一个model
-3 再用上面的model去预测我accept进来的房源，
-  如果accept了几条房源的话，看属于哪个类别的房源最多
+3 再用上面的model去预测历史浏览记录的房源，
+  看属于哪个类别的房源最多
 4 recommend函数就推荐那个最多类别的房源，推5条左右
 
 简单起见，这里只考虑，租金，装修，面积
@@ -53,11 +53,11 @@ X['装修'] = zx
 X['面积'] = X['面积'].apply(get_mianji)
 #X['年代'] = X['年代'].apply(get_niandai)
 #X['租赁方式'] = LabelEncoder().fit_transform(X['租赁方式'].values)
-X = X.dropna()
+#X = X.dropna()
 #print(X.head(50), X.shape)
 ss = StandardScaler()
 X2 = ss.fit_transform(X)
-kmeans = KMeans(n_clusters=15, max_iter=500)
+kmeans = KMeans(n_clusters=15, n_init=50)
 kmeans.fit(X2)
 
 #统计各个类别的数目
@@ -86,20 +86,61 @@ y = data['分类']
 knn = KNeighborsClassifier(n_neighbors=7)
 knn.fit(X, y)
 
+
+
+def _random_choice(lst, n):
+    '''从列表lst中随机选择n个不重复的元素'''
+    if n > len(lst):
+        return lst
+    choiced_elements = []
+    lst = lst.copy()
+    while n > 0:
+        element = choice(lst)
+        choiced_elements.append(element)
+        lst.remove(element) # 避免重复选择
+        n -= 1
+    return choiced_elements
+
+#rc = _random_choice([1,2,3,4,5,6,7], 2)
+#print(rc)
+
+
 # 假装这是某个用户最近的浏览历史记录
 viewed = [
-    [9900,  1, 50],
-    [9000,  4, 50],
-    [9000,  4, 50],
-    [9000,  3, 50],
-    [9200,  4, 60],
-    [9400,  4, 80],
-    [9000,  4, 50],
-    [9000,  4, 60],
-    [92000, 4, 120],
-    [9000,  4, 90],
-    [9500,  2, 27],
+    [9900,  1, 150],
+    [9000,  4, 150],
+    [9000,  4, 150],
+    [9000,  3, 150],
+    [9200,  4, 160],
+    [9400,  4, 180],
+    [9000,  4, 150],
+    [9000,  4, 160],
+    [92000, 4, 1120],
+    [9000,  4, 190],
+    [9500,  2, 127],
     ]
+
+def history_view():
+    
+    # 从csv文件中随机选择20条，假装是某个用户最近的浏览历史记录
+    choosed_idx = _random_choice(list(df.index), 20)
+    choosed_rows = df.ix[choosed_idx,:]
+    #print(choosed_rows)
+    X = choosed_rows.loc[:, ['租金', '装修', '面积']]
+    print('-----------------------------查看历史----------------------------------------------')
+    print(X)
+    zx = X['装修'].copy()
+    zx[zx=='毛坯'] = 1
+    zx[zx=='简单装修'] = 2
+    zx[zx=='中等装修'] = 3
+    zx[zx=='精装修'] = 4
+    zx[zx=='豪华装修'] = 5
+    X['装修'] = zx
+    X['面积'] = X['面积'].apply(get_mianji)
+    
+    return X
+#history_view()
+
 
 
 def recommend(viewed):
@@ -115,19 +156,22 @@ def recommend(viewed):
     recommended = data_recommend[data_recommend['分类'] == most_view]
     #idx = np.array([1,2,3,4,5])
     n = 5
-    if recommended.shape[0]>n: # 在这一类别里随机选5条，否则就全部选出
-        idx = list(recommended.index)
-        choiced_idx = []
-        while n > 0:
-            a = choice(idx)
-            choiced_idx.append(a)
-            idx.remove(a) # 避免重复选择
-            n -= 1
-    choiced_idx = np.array(choiced_idx)
+    #if recommended.shape[0]>n: # 在这一类别里随机选5条，否则就全部选出
+    #    idx = list(recommended.index)
+    #    choiced_idx = []
+    #    while n > 0:
+    #        a = choice(idx)
+    #        choiced_idx.append(a)
+    #        idx.remove(a) # 避免重复选择
+    #        n -= 1
+    ##choiced_idx = np.array(choiced_idx)
+    choiced_idx = _random_choice(list(recommended.index), n)
     #print(choiced_idx)
     recommended = recommended.ix[choiced_idx, :]
+    print('-----------------------------推荐的----------------------------------------------')
     print(recommended)
     return recommended
 
 
-recommend(viewed)
+#recommend(viewed)
+#recommend(history_view())
