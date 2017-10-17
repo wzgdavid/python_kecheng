2b2dc46ece1e9fc671e0c4319398154aed5aad74
wzgavid <wzgdavid@sina.com>
2017-10-17 18:06:23 +0800

    a

---
+++ samples/other_samples/sublime_build_system.py
@@ -0,0 +1,6 @@
+{
+    "cmd": ["D:/Python36/python3.exe","-u","$file"],
+    "file_regex": "^[ ]*File \"(...*?)\", line ([0-9]*)",
+    "selector": "source.python",
+    "encoding": "cp936"       // 加这一行
+}
