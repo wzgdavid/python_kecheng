7b86c7bf41bfc089fc1d7d08eb675432efb85bac
wzgdavid <wzgdavid@sina.com>
2021-01-16 20:11:29 +0800

    Update use_requests.py

--- samples/chapter8/use_requests.py
+++ samples/chapter8/use_requests.py
@@ -5,7 +5,7 @@ from lxml import etree
 #print( type(r.json()) )
 
 
-url = 'http://news.baidu.com/'
+url = 'http://s.weibo.com/weibo/%25E9%25A9%25AC%25E8%258B%258F%25E5%25B7%25A5%25E4%25BD%259C%25E5%25AE%25A4%25E5%25A3%25B0%25E6%2598%258E&page=3'
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36',
 }
@@ -20,5 +20,8 @@ html = etree.HTML(html)
 html = etree.ElementTree(html)
 
 
-btn = html.xpath('//*[@id="sugarea"]/span[2]/input/@value')
+
+btn = html.xpath('//*[@id="pl_weibo_direct"]/div/div[2]/div/div[1]/div/div[1]/dl/div/div[3]/div[1]/a[1]')
 print(btn)
+
+
